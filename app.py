079ef22aca8f90cabb3fa7e7ea627226b1a53db7
NISHKARSHrj <ronindas00@gmail.com>
2026-05-08 18:23:21 +0530

    deploying

--- app.py
+++ app.py
@@ -65,4 +65,4 @@ def process():
                      as_attachment=True, download_name='processed_images.zip')
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5000)
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000))) 
